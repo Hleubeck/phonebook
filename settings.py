66e6576dfbbaebcd5518d51c3d67438926400963
Hector Leubeck <112888659+Hleubeck@users.noreply.github.com>
2022-12-20 20:49:50 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%yke29)kmjy!c4rcz34*8j2ze$51n)o$%#q5db9%8n!l21(!w$'
+SECRET_KEY = "LOREM-IPSUM"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
